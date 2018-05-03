3ebb023fb5e9f17394834c9f4e288ad05e17bee1
pietvanzoen <pietvanzoen@gmail.com>
2018-05-03 18:27:58 +0200

    chore: fix git hooks install

--- scripts/git-hooks-install.sh
+++ scripts/git-hooks-install.sh
@@ -12,7 +12,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 repo_root=$(git rev-parse --show-toplevel)
 
 # find executable files in git-hooks folder
-executable_hooks=$(find $repo_root/scripts/git-hooks -type f -perm +111)
+executable_hooks=$(find $repo_root/scripts/git-hooks -type f -executable)
 
 # symlink them to the .git/hooks/ folder
 echo "$executable_hooks" | xargs -I {} sh -c 'ln -sf {} .git/hooks/$(basename {})'

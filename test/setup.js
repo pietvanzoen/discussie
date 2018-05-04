68c53e5de74d1a0c9476e7e9c8b30532f7b7de06
pietvanzoen <pietvanzoen@gmail.com>
2018-05-04 23:59:57 +0200

    chore: restore junit reporter

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,14 @@
-const logger = require('../lib/util/logger');
+// load env variables
+require('./_helpers/load-env')();
 
+if (process.env.JASMINE_JUNIT_PATH) {
+  const reporters = require('jasmine-reporters');
+  const junitReporter = new reporters.JUnitXmlReporter({
+    savePath: process.env.JASMINE_JUNIT_PATH,
+    consolidateAll: true,
+  });
+  jasmine.getEnv().addReporter(junitReporter);
+}
+
+const logger = require('../lib/util/logger');
 logger.level('warn');
